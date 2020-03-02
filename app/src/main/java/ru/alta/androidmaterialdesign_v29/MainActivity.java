e317508049326208dc88b7186d3d76dbbb381285
ganiushina <32090008+ganiushina@users.noreply.github.com>
2020-03-02 11:31:05 +0400

    Добавила цвета в новый стиль

--- app/src/main/java/ru/alta/androidmaterialdesign_v29/MainActivity.java
+++ app/src/main/java/ru/alta/androidmaterialdesign_v29/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setTheme(R.style.NewStyle);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
